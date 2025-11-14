3f650186b7a8b40318990f9895c372f85b89e66a
EditonR <editonrenne1997@hotmail.com>
2025-11-14 20:00:54 -0300

    Seção: Funções

---
+++ funcoes.js
@@ -0,0 +1,18 @@
+// Funções é um bloco de código que chamamos para ser executado.
+
+// Uma função precisa ser chamada
+//Pode ou não receber parâmetros
+//Pode ou não retornar dados
+
+function gerarNome() {
+  const nome = 'Eddie'
+  const sobrenome = 'Silva'
+  const idade = 27
+
+  console.log( 'Nome: ',  nome  ,  sobrenome )
+  console.log('Idade: ', idade)
+}
+
+gerarNome()
+
+//Serve para temos bloco de código p/ ser executados.
