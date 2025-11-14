21280cc232d39590dcfe63615518a7ab5e7d34b6
EditonR <editonrenne1997@hotmail.com>
2025-11-14 12:45:13 -0300

    javascript para testers

---
+++ variaveis.js
@@ -0,0 +1,30 @@
+//Variável é um endereço de memória onde tem um valor.
+//Ele pode ser alterado!
+var nome = 'Eddie' // string == texto
+nome = 'Eduardo'
+console.log(nome)
+
+//Ela pode ser alterado!
+let idade = 20 // numeros inteiros ou float
+
+console.log(idade)
+
+//NÃO pode ser alterado!
+const cidade ='São Paulo' 
+
+var maior_idade = true // boolean - verdadeiro ou falso
+
+const paises = ['Brasil', 'Argentina', 'Canada'] // lista de dados / arrays]
+
+//Onjeto
+let devices = {
+  nome: 'Iphone 17',
+  preco: 'R$10000',
+  cores: {
+    cor1:'verde',
+    cor2:'azul',
+  }
+}  
+
+//função
+
