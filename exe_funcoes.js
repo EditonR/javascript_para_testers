f7b0092795faf5476e84eb163f86e9ab938ce949
EditonR <editonrenne1997@hotmail.com>
2025-11-14 20:59:35 -0300

    Exercício 3 - Funções

---
+++ exe_funcoes.js
@@ -0,0 +1,22 @@
+// Exercício 3 - Funções
+
+//Criar uma função
+//Soma 2 números e retorna esses numero somado
+//Deve ser enviado por parametros os 2 números da conta
+
+// Numero1: 20
+//Numero2:10
+//Soma: 30
+//--------------------------------------
+
+function soma(numero1 , numero2) {
+  const resultado = numero1 + numero2
+
+  console.log('Numero1: ', 20)
+  console.log('Numero2: ', 10)
+  console.log('Soma: ', resultado)
+
+  
+}
+
+soma(20, 10)
