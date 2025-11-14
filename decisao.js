fa85cce12ae3dc698d162e72375d1f1ce4156535
EditonR <editonrenne1997@hotmail.com>
2025-11-14 14:38:24 -0300

    Conceito básicos - Estrutura de decisões

---
+++ decisao.js
@@ -0,0 +1,38 @@
+/* Estrutura de decisão, nosso famoso IF ELSE
+
+if (condição){
+  //aqui pode ter várias linha de códigos, chamamos de contextos.
+} else {
+
+}
+  */
+
+const nome = 'Eddie'
+
+// comparação de string console.log(nome == 'Eddie')
+
+/*if(nome =='eddie') {
+  console.log('Condição atendida!')
+} else {
+  console.log('Condição NÃO atendida!')
+}
+*/
+let idade = 10
+/*
+if (idade == 30){
+  console.log('Idade 28!')
+} else if (idade == 20) {
+  console.log('Idade 20!')
+} else {
+  console.log('Nenhuma condição atendida!')
+}
+  */
+
+// Estrutura de decisão - Boolean
+const e_maior_idade = false
+
+if(e_maior_idade) {
+  console.log('É maior de idade!')
+} else {
+  console.log('NÃO é maior de idade!')
+}
